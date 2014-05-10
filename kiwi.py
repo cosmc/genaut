f7465dcce4f2702b0699d0a7b330d5fba57c6835
Kiwako Sakamoto <kiwako.sakamoto@gmail.com>
2014-05-10 02:41:06 -0500

    Modified to output the plots and the maximum scores ONLY WHEN there is a change from the previous generaion.

--- kiwi.py
+++ kiwi.py
@@ -1,10 +1,15 @@
+#!/usr/local/bin/python
+
 from gautils import *
-from evalkiwi import *
+from ca_eval import *
 from spect import *
 import matplotlib.pyplot as plt
 import os
+from datetime import datetime
 
 import random
+
+plotDirectory = 'plots/'+datetime.now().strftime('%Y%m%d_%H%M')+'/'
 
 random.seed(42)
 
@@ -18,23 +23,25 @@ perf = getPerfectPattern('./ploink.wav', timesteps, width, 1.0)
 plt.matshow(perf, cmap=cm.gist_heat_r)
 if not os.path.exists('plots'):
 	os.makedirs('plots')
-plt.savefig('plots/perf.png')
+os.makedirs(plotDirectory)
+plt.savefig(plotDirectory+'perf.png')
 plt.close
 
+previousBest = 0
 for gen in xrange(numGens):
-
 	fitpop = evalFitness(population, perf, timesteps, width, 3)
-
 	#top fitness score
-	best = max(fitpop, key=lambda x: x[1])
-	print gen ,":", best[1]
-	fname = "plots/" + str(gen) + ".png"
-	fname_sub = "plots/" + str(gen) + "_sub.png"
-	plt.matshow(best[2], cmap=cm.gist_heat_r)
-	plt.savefig(fname)
-	plt.close()
-	plt.matshow(best[3], cmap=cm.gist_heat_r)
-	plt.savefig(fname_sub)
-	plt.close()
+	best = max(fitpop, key=lambda x: x[1])	
+	if best[1] != previousBest:
+		print gen ,":", best[1]
+		fname = plotDirectory + str(gen) + ".png"
+		fname_sub = plotDirectory + str(gen) + "_sub.png"
+		plt.matshow(best[2], cmap=cm.gist_heat_r)
+		plt.savefig(fname)
+		plt.close()
+		plt.matshow(best[3], cmap=cm.gist_heat_r)
+		plt.savefig(fname_sub)
+		plt.close()
 
+		previousBest = best[1]
 	population = breed(fitpop, 0.2, 0.0, 0.05)
